edb31e3a453230d91dfea024003973467981d0ee
nultinator <72562693+nultinator@users.noreply.github.com>
2021-08-11 23:22:04 -0400

    Add files via upload

---
+++ ycash.jl
@@ -0,0 +1,17 @@
+
+
+import Pkg
+Pkg.add("JSON")
+import JSON
+using JSON
+
+pwd()
+cd("ycash-441/src")
+function ycashcli(command)
+	JSON.parse(read(`./ycash-cli $command`, String))
+end
+addresses = JSON.parse(read(`./ycash-cli z_listaddresses`, String))
+notes = JSON.parse(read(`./ycash-cli z_listunspent`, String))
+memos = String(hex2bytes(notes[1]["memo"])) #FIX THE KEY VARIABLE IN THIS LINE!!!!!
+
+
