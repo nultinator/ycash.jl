9acce4563575fff22c71ba01426129a666a99b63
nultinator <72562693+nultinator@users.noreply.github.com>
2021-08-17 10:32:54 -0400

    Add files via upload

--- ycash.jl
+++ ycash.jl
@@ -6,12 +6,28 @@ import JSON
 using JSON
 
 pwd()
-cd("ycash-441/src") #Enter the path to your ycash node here
+println("Enter the path to ycash/zcash-cli")
+location = readline()
+cd(location)
 function ycashcli(command)
 	JSON.parse(read(`./ycash-cli $command`, String))
 end
 addresses = JSON.parse(read(`./ycash-cli z_listaddresses`, String))
 notes = JSON.parse(read(`./ycash-cli z_listunspent`, String))
 memos = String(hex2bytes(notes[1]["memo"])) #FIX THE KEY VARIABLE IN THIS LINE!!!!!
+#This send function doesn't work correctly yet, but it's close
+function z_sendtofrom()
+           println("Send from?")
+           from = readline()
+           println("Send to?")
+           to = readline()
+           println("How much?")
+           amount = readline()
+           println("Memo?")
+           memo = readline()
+           dict = Dict("address" => "$to", "amount" => amount, "memo" => "$memo")
+           params = JSON.json(dict)
+           run(`./ycash-cli z_sendmany "$from" 'params'`)
+end
 
 
